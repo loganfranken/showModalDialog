17c22c327bfbbe9ccd3ab50d71e909384661aa9e
stnbip <stnbip@gmail.com>
2014-09-10 16:32:16 -0700

    Changing how iFrame's window receives focus.
    * Changing how the iFrame's window receives focus.
    * Moving the close button's HTML to a variable; hopefully to improve readability.

--- showModalDialog.js
+++ showModalDialog.js
@@ -1,55 +1,55 @@
 (function () {
- 
     if (parent && parent.closeModal) {
         window.close = function () {
             try { parent.closeModal(); } catch (ex) { }
         };
     }
- 
+
     window.showModalDialog = window.showModalDialog || function (url, arg, opt) {
         url = url || ''; //URL of a dialog
         arg = arg || null; //arguments to a dialog
         opt = opt || 'dialogWidth:300px;dialogHeight:200px'; //options: dialogTop;dialogLeft;dialogWidth;dialogHeight or CSS styles
- 
+
         var dialog = document.body.appendChild(document.createElement('dialog'));
- 
+
         // Clean up height and width. (dialogHeight/dialogheight > height, dialogWidth/dialogwidth > width).
         var newStyle = opt.replace('dialogh', 'h').replace('dialogH', 'h').replace('dialogw', 'w').replace('dialogW', 'w');
- 
+
         // Add a trailing semi-colon if necessary.
         if (newStyle.length > 0 && newStyle.slice(-1) !== ';') {
             newStyle += ';';
         }
- 
+
         // Apply style and center the dialog.
         dialog.setAttribute('style', newStyle + 'margin:0 auto;');
- 
-        dialog.innerHTML = '<a href="#" id="dialog-close" style="position: absolute; top: 0; right: 4px; font-size: 20px; color: #000; text-decoration: none; outline: none;">&times;</a><iframe id="dialog-body" src="' + url + '" style="border: 0; width: 100%; height: 100%;"></iframe>';
+
+        var closeButtonHtml = '<a href="#" id="dialog-close" style="position: absolute; top: 0; right: 4px; font-size: 20px; color: #000; text-decoration: none; outline: none;">&times;</a>';
+
+        // Focus on the content window when the iFrame is loaded.
+        dialog.innerHTML = closeButtonHtml + '<iframe id="dialog-body" onload="this.contentWindow.focus();" src="' + url + '" style="border: 0; width: 100%; height: 100%;"></iframe>';
+
         document.getElementById('dialog-body').contentWindow.dialogArguments = arg;
- 
-        // Focus on the inner iFrame.
-        document.getElementById('dialog-body').focus();
- 
+
         document.getElementById('dialog-close').addEventListener('click', function (e) {
             e.preventDefault();
             dialog.close();
         });
         dialog.showModal();
- 
+
         var documentHtml = document.documentElement.innerHTML;
         var regex = /<script>window.showModalDialog\((.*?)\);(.*?)<\/script>/g;
         var match = regex.exec(documentHtml);
         var nextStatement = match[2];
- 
+
         dialog.addEventListener('close', function () {
             document.getElementById('dialog-body').contentWindow.returnValue;
             document.body.removeChild(dialog);
             eval(nextStatement);
         });
- 
+
         window.closeModal = function () { dialog.close(); }
- 
+
         throw 'Execution stopped until showModalDialog is closed';
     };
- 
+
 })();
